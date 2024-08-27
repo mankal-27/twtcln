59454105164ccd76eea0efdabd0dd044e5a4dbea
mankal-27 <mnkalkutagi@gmail.com>
2024-08-27 15:08:39 +0530

    First Commit with Structure Created

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+            data: "You Hit the Sign Up Endpoint"
+    })
+}
+
+export const login = async (req, res) => {
+    res.json({
+            data: "You Hit the login Endpoint"
+    })
+}
+
+export const logout = async (req, res) => {
+    res.json({
+            data: "You Hit the logoutEndpoint"
+    })
+}
